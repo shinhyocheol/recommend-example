1124f172c8c34f3856b0e3b123d7090f936f5ca4
shinhyocheol <zzzskssk1@naver.com>
2022-08-01 19:02:41 +0900

    # spring security 추가

---
+++ src/main/java/com/example/app/configuration/constant/Role.java
@@ -0,0 +1,6 @@
+package com.example.app.configuration.constant;
+
+public interface Role {
+    String DABANG = "ROLE_DABANG";
+    String PRO = "ROLE_PRO";
+}
--- src/main/java/com/example/app/ui/request/RequestApiController.java
+++ src/main/java/com/example/app/ui/request/RequestApiController.java
@@ -2,12 +2,14 @@ package com.example.app.ui.request;
 
 import com.example.app.application.RequestsFacade;
 import com.example.app.common.response.CommonResponse;
+import com.example.app.configuration.constant.Role;
 import com.example.app.domain.request.RequestInfo;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +24,7 @@ public class RequestApiController {
     private final RequestsFacade facade;
     private final RequestMapStruct mapStruct;
 
+    @Secured({Role.DABANG})
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addRecommendRequest(@RequestBody @Valid RequestDto.AddParam param) {
         facade.addRequest(mapStruct.of(param));
